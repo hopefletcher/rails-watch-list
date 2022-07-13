904cebae1707b6fd6a7ef37115c97b1106cc7018
Hope Fletcher <thehopefletcher@gmail.com>
2022-07-13 17:07:38 +0200

    db's in place

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_09_113652) do
+ActiveRecord::Schema.define(version: 2022_07_13_150704) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "bookmarks", force: :cascade do |t|
-    t.text "comment"
     t.bigint "movie_id", null: false
     t.bigint "list_id", null: false
+    t.text "comment"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["list_id"], name: "index_bookmarks_on_list_id"
